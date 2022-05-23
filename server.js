7fea97d1cf569af6abea03f35658f65213952612
Vinicius de Jesus Tamura <vtamura@cervejariacidadeimperial.com.br>
2022-05-23 17:54:42 -0300

    Creating post and get methods.

--- server.js
+++ server.js
@@ -1,19 +1,6 @@
-const http = require('http')
-const port = 3001
+import app from './src/app.js'
+const port = process.env.PORT || 3001
 
-
-const routes = {
-    '/': 'Página Inicial',
-    '/livros': 'Página de Livros',
-    '/autores': 'Página de Autores',
-    '/sobre': 'Info sobre o site.'
-}
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'content-type': 'text/plain'})
-    res.end(routes[req.url])
-})
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Servidor iniciado no endereço http://localhost:${port}`)
 })
---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const livros = [
+    { id: 1, titulo: "Senhor dos Aneis"},
+    { id: 2, titulo: "Hobbit"}
+]
+
+app.get('/', (req, res) => {
+    res.status(200).send('Curso Node')
+})
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros)
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body)
+    res.status(201).send('Livro cadastrado com sucesso.')
+})
+
+export default app
